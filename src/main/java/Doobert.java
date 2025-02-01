48b9944727bcbed76917f362c35577a268c4529c
cj143n <chuaje99@gmail.com>
2025-02-02 00:14:16 +0800

    Allow adding user input into list

--- src/main/java/Doobert.java
+++ src/main/java/Doobert.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 class Doobert{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        String[] listOfTasks = new String[100];
+        int count = 1;
 
         System.out.println("Hello! I'm Doobert!");
         System.out.println("What can I do for you?");
 
         String line = input.nextLine();
-        while (!line.equals("bye")) {
-            System.out.println(line);
+        while (!line.equals("bye") && count <= 100) {
+            if (line.equals("list")) { //lists out todo list
+                for (int i = 0; i < count-1; i++) {
+                    System.out.println(listOfTasks[i]);
+                }
+            } else {
+                System.out.println("added: " + line); //adding task into list
+                listOfTasks[count-1] = (count + ". " + line);
+                count++;
+            }
             line = input.nextLine();
         }
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye! See you later alligator");
     }
 }
