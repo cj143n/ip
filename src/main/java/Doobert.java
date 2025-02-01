7927375135805abb6c7227fcdfaad8c462c93fd9
cj143n <chuaje99@gmail.com>
2025-02-01 23:35:25 +0800

    Create echoing of user input

--- src/main/java/Doobert.java
+++ src/main/java/Doobert.java
@@ -1,7 +1,18 @@
-public class Doobert{
+import java.util.Scanner;
+
+class Doobert{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hello! I'm Doobert!");
         System.out.println("What can I do for you?");
+
+        String line = input.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            line = input.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
