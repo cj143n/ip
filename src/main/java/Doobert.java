1dc7e0ee56b0f42b23611e605c6fbf3c16d86340
cj143n <chuaje99@gmail.com>
2025-02-02 01:32:14 +0800

    Add mark as done functionality and created Task class

--- src/main/java/Doobert.java
+++ src/main/java/Doobert.java
@@ -1,9 +1,46 @@
 import java.util.Scanner;
 
-class Doobert{
-    public static void main(String[] args){
+class Doobert {
+    public static void listTasks(Task[] listOfTasks, int count) {
+        if (count == 1) {
+            System.out.println("No tasks yet!");
+            return;
+        }
+        for (int i = 0; i < count-1; i++) {
+            System.out.println((i+1) + "." + listOfTasks[i].output());
+        }
+    }
+
+    public static void markTask(Task[] listOfTasks, int count, String line) {
+        int taskNo = Integer.parseInt(line.substring(5));
+        if (taskNo < 1 || taskNo >= count) {
+            System.out.println("Invalid task number, please try again");
+            return;
+        }
+        listOfTasks[taskNo - 1].markDone();
+        System.out.println("Task successfully marked!");
+        System.out.println(listOfTasks[taskNo - 1].output());
+    }
+
+    public static void unmarkTask(Task[] listOfTasks, int count, String line) {
+        int taskNo = Integer.parseInt(line.substring(7));
+        if (taskNo < 1 || taskNo >= count) {
+            System.out.println("Invalid task number, please try again");
+            return;
+        }
+        listOfTasks[taskNo - 1].markUndone();
+        System.out.println("Task successfully unmarked!");
+        System.out.println(listOfTasks[taskNo - 1].output());
+    }
+
+    public static void addTask(Task[] listOfTasks, int count, String line) {
+        System.out.println("added: " + line);
+        listOfTasks[count-1] = new Task(line);
+    }
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[] listOfTasks = new String[100];
+        Task[] listOfTasks = new Task[100];
         int count = 1;
 
         System.out.println("Hello! I'm Doobert!");
@@ -12,17 +49,19 @@ class Doobert{
         String line = input.nextLine();
         while (!line.equals("bye") && count <= 100) {
             if (line.equals("list")) { //lists out todo list
-                for (int i = 0; i < count-1; i++) {
-                    System.out.println(listOfTasks[i]);
-                }
+                listTasks(listOfTasks, count);
+            } else if (line.matches("^mark \\d+$")) {
+                markTask(listOfTasks, count, line);
+            } else if (line.matches("^unmark \\d+$")) {
+                unmarkTask(listOfTasks, count, line);
             } else {
-                System.out.println("added: " + line); //adding task into list
-                listOfTasks[count-1] = (count + ". " + line);
+                addTask(listOfTasks, count, line);
                 count++;
             }
             line = input.nextLine();
         }
 
         System.out.println("Bye! See you later alligator");
+        input.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatusIcon() {
+        return (done ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void markUndone() {
+        this.done = false;
+    }
+
+    public String output() {
+        return "[" + getStatusIcon() + "] " + getDescription();
+    }
+}
